5660c45bca12bb332d888ee07f470f9e18844248
Takumi Nagashima <emperte2008@gmail.com>
2022-09-11 16:51:11 +0900

    ログイン後遷移変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+# サインイン後のリダイレクト先をマイページへ
+def after_sign_in_path_for(resource)
+    user_path(current_user.id)  
+end
+
+# サインアウト後のリダイレクト先をトップページへ
+def after_sign_out_path_for(resource)
+    new_user_session_path
+end
+
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :family_id]) 
         devise_parameter_sanitizer.permit(:invite) { |u| u.permit(:email, :name, :family_id) }
