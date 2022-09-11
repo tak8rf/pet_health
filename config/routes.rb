dbb837df16c82a9f072c620ed1ee8de1cd69e6e8
Takumi Nagashima <emperte2008@gmail.com>
2022-09-11 16:24:20 +0900

    ログイン遷移先変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  resources :healths
-  resources :pets
-  resources :families
   get 'users/show'
-  root to: "families#new"
-  resources :tasks
-
   devise_for :users, controllers: {
     invitations: 'users/invitations'
   }
   resources :users, only: :show
+  
+  resources :healths
+  resources :pets
+  resources :families
+  resources :tasks
+  
+  root to: "healths#index"
+  
   
   post '/tasks/:id/done' => 'tasks#done',   as: 'done'
   get 'search' => 'healths#search'
